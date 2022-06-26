aa55e8bc76bf8322e9fc45134da1d20deaaa9b7a
zFl4wless <noah.bauditz@live.de>
2022-06-26 11:27:22 +0200

    Adds counter styling and the count state

--- src/components/counter.tsx
+++ src/components/counter.tsx
@@ -1,9 +1,19 @@
-import { Component } from 'react'
- 
+import { Component } from 'react';
+
 class Counter extends Component {
-    render() { 
-        return <h1>Counter Component</h1>;
+    state = {
+        count: 0
+    };
+
+    render() {
+        return (
+            <div className='py-2 text-center'>
+                <button className='btn btn-primary'>+</button>
+                <h1>{this.state.count}</h1>
+                <button className='btn btn-danger'>-</button>
+            </div>
+        );
     }
 }
- 
+
 export default Counter;
